559418a2c2e437714ee726202b5907b50a01d963
Zack Jameson <zacharymjameson@gmail.com>
2021-04-14 19:04:51 -0400

    commentsrouter and service

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const articlesRouter = require("./articles/articles-router");
 const usersRouter = require("./users/users-router");
+const commentsRouter = require("./comments/comments-router");
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(helmet());
 
 app.use("/api/articles", articlesRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/comments", commentsRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
