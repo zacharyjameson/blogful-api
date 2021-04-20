c9834a529b1b2d0d3af003a4a64b648908a80ca8
Zack Jameson <zacharymjameson@gmail.com>
2021-04-19 21:07:20 -0400

    ssl auth

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/blogful',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder-mifflin@localhost/blogful-test'
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder-mifflin@localhost/blogful-test',
+  SSL: true,
 }
